0733f656eef36197c87585877d412b230c109151
Dan Haynes <dhaynes@inspec-dev.mitre.org>
2018-03-23 10:25:02 -0400

    Updated to use proper filter table operations.

--- controls/V-72023.rb
+++ controls/V-72023.rb
@@ -60,7 +60,7 @@ Note: The example will be for the user smithj, who has a home directory of
 # chown smithj /home/smithj/<file or directory>"
   #Get home directory from /etc/passwd. Check users with UID >= 1000.
   findings = []
-  u = users.where{uid >= 1000 and home.empty?}.entries
+  u = users.where{uid >= 1000 and home != ""}.entries
   #For each user, build and execute a find command that identifies files
   #that are not owned by the user.
   u.each do |user|
