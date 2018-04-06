4089ff592d544a475b23166cbdf014db95c0c38c
Aaron Lippold <lippold@gmail.com>
2018-04-06 09:04:22 -0400

    Merge DEV2 into DEV (#10)
    * * Reviewed all controls and fixed many issues
    * updated impact logic to account for Not Applicable cases
    * used the protected regex pattern `%r{ }` in place is //
    * fixed many false positives reported by end users
    * added TODOs to all controls that need to be enhanced
    * added a new attributes files for this branch for review
    * added logic so that all controls correctly report
    * removed most `only_if` statments and replaced with skips + impact 0.0
    * removed all `severity` tags in lue of using `impact`
    
    Signed-off-by: Aaron Lippold <lippold@gmail.com>
    
    * * Added EXEMPT_HOME_USERS attribute attributes file
    * Removed extra attribute defn
    * Fixed typo in call to package resource
    * Updated attributes file
    
    Signed-off-by: Bradley Shelton <bsheltonihs@gmail.com>
    
    * small change to push file
    
    Signed-off-by: Aaron Lippold <lippold@gmail.com>
    
    * * updated V-71863 fix the banner compare
    * updated V-71961 to account for multiple users
    * added attrubutes to support each
    
    Signed-off-by: Bradley Shelton <bsheltonihs@gmail.com>

--- controls/V-72231.rb
+++ controls/V-72231.rb
@@ -33,7 +33,7 @@ distribution of the public key to verify the hash information while maintaining 
 confidentiality of the key used to generate the hash.
   "
   impact 0.5
-  tag "severity": "medium"
+
   tag "gtitle": "SRG-OS-000250-GPOS-00093"
   tag "gid": "V-72231"
   tag "rid": "SV-86855r2_rule"
@@ -75,7 +75,7 @@ the X.509 certificates used for peer authentication."
     end
     # @todo - pam resource - also dynamically find directory?
     describe command('grep -i cacertfile /etc/pam_ldap.conf') do
-      its('stdout.strip') { should match /^tls_cacertfile \/etc\/openldap\/ldap-cacert.pem$/}
+      its('stdout.strip') { should match %r{^tls_cacertfile /etc/openldap/ldap-cacert.pem$} }
     end
   end
 
