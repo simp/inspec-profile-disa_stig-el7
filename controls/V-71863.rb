b02223a5b485805edcde3e3c48c32d09104cbba9
root <root@inspec-dev.mitre.org>
2018-04-16 09:00:35 -0400

    Updated control to also check if the file exists.

--- controls/V-71863.rb
+++ controls/V-71863.rb
@@ -22,15 +22,20 @@ uri: http://iase.disa.mil
 
 #TODO: Make sure this is acctually an onlyif on the GUI - ssh banner, ftp banner also use /etc/issue
 
+#BANNER_MESSAGE_TEXT_CLI = attribute('banner_message_text_cli',
+#  default: "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
+#  By using this IS (which includes any device attached to this IS), you consent to the following conditions:
+#    - The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.
+#    - At any time, the USG may inspect and seize data stored on this IS.
+#    - Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.
+#    - This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy.
+ #   - Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details.",
+#  description: 'The banner message  must display the Standard Mandatory DoD Notice and Consent Banner before granting access.')
+
 BANNER_MESSAGE_TEXT_CLI = attribute('banner_message_text_cli',
-  default: "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
-  By using this IS (which includes any device attached to this IS), you consent to the following conditions:
-    - The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.
-    - At any time, the USG may inspect and seize data stored on this IS.
-    - Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.
-    - This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy.
-    - Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details.",
-  description: 'The banner message  must display the Standard Mandatory DoD Notice and Consent Banner before granting access.')
+  default:"You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only. By using this IS (which includes any device attached to this IS), you consent to the following conditions: -The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations. -At any time, the USG may inspect and seize data stored on this IS. -Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose. -This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy. -Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details.",
+description: 'The banner message  must display the Standard Mandatory DoD Notice and Consent Banner before granting access.')
+
 
 control "V-71863" do
   title "The operating system must display the Standard Mandatory DoD Notice and
@@ -159,10 +164,17 @@ communications, or work product, related to personal representation or services 
 attorneys, psychotherapists, or clergy, and their assistants. Such communications
 and work product are private and confidential. See User Agreement for details.\""
 
+  CLEAN_BANNER = BANNER_MESSAGE_TEXT_CLI.gsub(%r{[\r\n\s]}, '')
+  banner_file = file("/etc/issue")
+  BANNER_MISSING = !banner_file.exist?
 
-  CLEAN_BANNER = BANNER_MESSAGE_TEXT_CLI.gsub(%r{[\s]}, '')
+  describe "The banner text is not set because /etc/issue does not exist" do
+    subject { BANNER_MISSING }
+    it { should be false }
+  end if BANNER_MISSING
+
   describe "The banner test should work" do
-   subject { file("/etc/issue").content.gsub(%r{[\s]}, '') }
+    subject { banner_file.content.gsub(%r{[\r\n\s]}, '') }
     it { should cmp CLEAN_BANNER }
-  end
+  end if !BANNER_MISSING
 end
