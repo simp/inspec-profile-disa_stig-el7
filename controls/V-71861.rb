a8cc5f152158a285b7e41b2800b94de63746bae2
root <root@inspec-dev.mitre.org>
2018-04-16 11:37:03 -0400

    Updated to check for both long/short banner text as well as report a finding when the banner text is not found.

--- controls/V-71861.rb
+++ controls/V-71861.rb
@@ -21,14 +21,14 @@ uri: http://iase.disa.mil
 =end
 
 BANNER_MESSAGE_TEXT_GUI = attribute('banner_message_text_gui',
-default: "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
-By using this IS (which includes any device attached to this IS), you consent to the following conditions:
-  - The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.
-  - At any time, the USG may inspect and seize data stored on this IS.
-  - Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.
-  - This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy.
-  - Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details.",
+default: "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only. By using this IS (which includes any device attached to this IS), you consent to the following conditions: -The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations. -At any time, the USG may inspect and seize data stored on this IS. -Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose. -This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy. -Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details.",
 description: 'The banner message  must display the Standard Mandatory DoD Notice and Consent Banner before granting access.')
+
+BANNER_MESSAGE_TEXT_GUI_LIMITED = attribute('banner_message_text_gui_limited',
+default: "I've read & consent to terms in IS user agreem't.",
+description: 'The banner message must display the Standard Mandatory DoD Notice and Consent Bann\
+er before granting access.')
+
 
 control "V-71861" do
   title "The operating system must display the approved Standard Mandatory DoD
@@ -167,18 +167,43 @@ communications, or work product, related to personal representation or services 
 attorneys, psychotherapists, or clergy, and their assistants. Such communications
 and work product are private and confidential. See User Agreement for details.’"
 
-  describe.one do
-    describe command("grep banner-message-text /etc/dconf/db/local.d/*") do
-      its('stdout') { should cmp BANNER_MESSAGE_TEXT_GRAPHICAL_GUI }
-    end
+  #Get all files that have the banner-message-text specified.
+  banner_files = 
+    command("grep -l banner-message-text /etc/dconf/db/local.d/*").stdout.split("\n") 
+  
+  #If there are no banner files then this is a finding.
+  BANNER_MISSING = banner_files.empty?
+  describe "If no files specify the banner text then this is a finding" do
+    subject { BANNER_MISSING }
+    it{should be false}
+  end if BANNER_MISSING
 
-    describe command("grep banner-message-text /etc/dconf/db/local.d/*") do
-      its('stdout') { should cmp "banner-message-text='I\'ve read & consent to terms in IS user agreem\'t.'"}
+  #If there are banner files then check them to make sure they have the correct text.
+  banner_files.each do |banner_file|
+    banner_message = 
+      parse_config_file(banner_file).params("banner-message-text").gsub(%r{[\r\n\s]}, '')
+    #dconf expects the banner-message-text to be quoted so remove leading and trailing quote.
+    #See https://developer.gnome.org/dconf/unstable/dconf-tool.html which states: 
+    #  VALUE arguments must be in GVariant format, so e.g. a string must include 
+    #  explicit quotes: "'foo'". This format is also used when printing out values. 
+    if banner_message.start_with?('"') || banner_message.start_with?('\'')
+      banner_message = banner_message[1,banner_message.length]
+    end
+    if banner_message.end_with?('"') || banner_message.end_with?('\'')
+      banner_message = banner_message.chop
+    end
+    describe.one do 
+      describe banner_message do
+        it{should cmp BANNER_MESSAGE_TEXT_GUI.gsub(%r{[\r\n\s]}, '')} 
+      end
+      describe banner_message do
+        it{should cmp BANNER_MESSAGE_TEXT_GUI_LIMITED.gsub(%r{[\r\n\s]}, '')}
+      end
     end
   end if package('gnome-desktop3').installed?
-
-    describe "The system does not have GNOME installed" do
-      skip "The system does not have GNOME installed, this requirement is Not
+  
+  describe "The system does not have GNOME installed" do
+    skip "The system does not have GNOME installed, this requirement is Not
       Applicable."
-    end if !package('gnome-desktop3').installed?
+  end if !package('gnome-desktop3').installed?
 end
