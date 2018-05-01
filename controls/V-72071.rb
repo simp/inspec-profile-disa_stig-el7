e3b4ffdbc688aca37c5536e1175b5fff0ebf3561
ejaronne <34140975+ejaronne@users.noreply.github.com>
2018-05-01 11:38:40 -0400

    Update to RHEL7v4
    - Update from RHEL7 Rel1 to Rel4

--- controls/V-72071.rb
+++ controls/V-72071.rb
@@ -1,50 +1,31 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
-
 control "V-72071" do
-  title "The file integrity tool must be configured to verify extended attributes."
-  desc  "Extended attributes in file systems are used to contain arbitrary data and
-file metadata with security implications."
+  title "The file integrity tool must be configured to verify extended
+attributes."
+  desc  "Extended attributes in file systems are used to contain arbitrary data
+and file metadata with security implications."
   impact 0.3
-
   tag "gtitle": "SRG-OS-000480-GPOS-00227"
   tag "gid": "V-72071"
   tag "rid": "SV-86695r2_rule"
   tag "stig_id": "RHEL-07-021610"
-  tag "cci": "CCI-000366"
+  tag "cci": ["CCI-000366"]
+  tag "documentable": false
   tag "nist": ["CM-6 b", "Rev_4"]
   tag "check": "Verify the file integrity tool is configured to verify extended
 attributes.
 
-Check to see if Advanced Intrusion Detection Environment (AIDE) is installed on the
-system with the following command:
+Check to see if Advanced Intrusion Detection Environment (AIDE) is installed on
+the system with the following command:
 
 # yum list installed aide
 
-If AIDE is not installed, ask the System Administrator how file integrity checks are
-performed on the system.
+If AIDE is not installed, ask the System Administrator how file integrity
+checks are performed on the system.
 
-If there is no application installed to perform file integrity checks, this is a
-finding.
+If there is no application installed to perform file integrity checks, this is
+a finding.
 
 Note: AIDE is highly configurable at install time. These commands assume the
 \"aide.conf\" file is under the \"/etc\" directory.
@@ -53,8 +34,8 @@ Use the following command to determine if the file is in another location:
 
 # find / -name aide.conf
 
-Check the \"aide.conf\" file to determine if the \"xattrs\" rule has been added to
-the rule list being applied to the files and directories selection lists.
+Check the \"aide.conf\" file to determine if the \"xattrs\" rule has been added
+to the rule list being applied to the files and directories selection lists.
 
 An example rule that includes the \"xattrs\" rule follows:
 
@@ -63,13 +44,14 @@ All= p+i+n+u+g+s+m+S+sha512+acl+xattrs+selinux
 /sbin All          # apply the same custom rule to the files in sbin
 
 If the \"xattrs\" rule is not being used on all selection lines in the
-\"/etc/aide.conf\" file, or extended attributes are not being checked by another
-file integrity tool, this is a finding."
-  tag "fix": "Configure the file integrity tool to check file and directory extended
-attributes.
+\"/etc/aide.conf\" file, or extended attributes are not being checked by
+another file integrity tool, this is a finding."
+  tag "fix": "Configure the file integrity tool to check file and directory
+extended attributes.
 
 If AIDE is installed, ensure the \"xattrs\" rule is present on all file and
 directory selection lists."
+  tag "fix_id": "F-78423r1_fix"
 
   describe package("aide") do
     it { should be_installed }
@@ -78,3 +60,4 @@ directory selection lists."
     it { should eq true }
   end
 end
+
--- controls/V-72091.rb
+++ controls/V-72091.rb
@@ -1,63 +1,47 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
-
 control "V-72091" do
-  title "The operating system must immediately notify the System Administrator (SA)
-and Information System Security Officer (ISSO) (at a minimum) via email when the
-threshold for the repository maximum audit record storage capacity is reached."
-  desc  "If security personnel are not notified immediately when the threshold for
-the repository maximum audit record storage capacity is reached, they are unable to
-expand the audit record storage capacity before records are lost."
+  title "The operating system must immediately notify the System Administrator
+(SA) and Information System Security Officer (ISSO) (at a minimum) via email
+when the threshold for the repository maximum audit record storage capacity is
+reached."
+  desc  "If security personnel are not notified immediately when the threshold
+for the repository maximum audit record storage capacity is reached, they are
+unable to expand the audit record storage capacity before records are lost."
   impact 0.5
-
   tag "gtitle": "SRG-OS-000343-GPOS-00134"
   tag "gid": "V-72091"
   tag "rid": "SV-86715r1_rule"
   tag "stig_id": "RHEL-07-030340"
-  tag "cci": "CCI-001855"
+  tag "cci": ["CCI-001855"]
+  tag "documentable": false
   tag "nist": ["AU-5 (1)", "Rev_4"]
   tag "subsystems": ['audit', 'auditd']
-  tag "check": "Verify the operating system immediately notifies the SA and ISSO (at
-a minimum) via email when the allocated audit record storage volume reaches 75
-percent of the repository maximum audit record storage capacity.
+  tag "check": "Verify the operating system immediately notifies the SA and
+ISSO (at a minimum) via email when the allocated audit record storage volume
+reaches 75 percent of the repository maximum audit record storage capacity.
 
-Check what action the operating system takes when the threshold for the repository
-maximum audit record storage capacity is reached with the following command:
+Check what action the operating system takes when the threshold for the
+repository maximum audit record storage capacity is reached with the following
+command:
 
 # grep -i space_left_action  /etc/audit/auditd.conf
 space_left_action = email
 
-If the value of the \"space_left_action\" keyword is not set to \"email\", this is a
-finding."
-  tag "fix": "Configure the operating system to immediately notify the SA and ISSO
-(at a minimum) when the threshold for the repository maximum audit record storage
-capacity is reached.
+If the value of the \"space_left_action\" keyword is not set to \"email\", this
+is a finding."
+  tag "fix": "Configure the operating system to immediately notify the SA and
+ISSO (at a minimum) when the threshold for the repository maximum audit record
+storage capacity is reached.
 
-Uncomment or edit the \"space_left_action\" keyword in \"/etc/audit/auditd.conf\"
-and set it to \"email\".
+Uncomment or edit the \"space_left_action\" keyword in
+\"/etc/audit/auditd.conf\" and set it to \"email\".
 
 space_left_action = email"
+  tag "fix_id": "F-78443r1_fix"
 
   describe auditd_conf do
     its('space_left_action.downcase') { should cmp 'email' }
   end
 end
+
