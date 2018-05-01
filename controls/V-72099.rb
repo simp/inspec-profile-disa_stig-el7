5b37bf0a01d8410d7ca5a13e7fa1e137f16c9d61
ejaronne <34140975+ejaronne@users.noreply.github.com>
2018-05-01 11:38:40 -0400

    Update to RHEL7v4
    - Update from RHEL7 Rel1 to Rel4

--- controls/V-72099.rb
+++ controls/V-72099.rb
@@ -1,42 +1,26 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
-
 control "V-72099" do
-  title "All uses of the `fchown` command must be audited."
-  desc  "Without generating audit records that are specific to the security and mission
-        needs of the organization, it would be difficult to establish, correlate, and
-        investigate the events relating to an incident or identify those responsible for one.
+  title "All uses of the fchown command must be audited."
+  desc  "
+    Without generating audit records that are specific to the security and
+mission needs of the organization, it would be difficult to establish,
+correlate, and investigate the events relating to an incident or identify those
+responsible for one.
 
-        Audit records can be generated from various components within the information
-        system (e.g., module or policy filter)."
-
+    Audit records can be generated from various components within the
+information system (e.g., module or policy filter).
+  "
   impact 0.5
-
   tag "gtitle": "SRG-OS-000064-GPOS-00033"
+  tag "satisfies": ["SRG-OS-000064-GPOS-00033", "SRG-OS-000392-GPOS-00172",
+"SRG-OS-000458-GPOS-00203", "SRG-OS-000474-GPOS-00219"]
   tag "gid": "V-72099"
-  tag "rid": "SV-86723r2_rule"
+  tag "rid": "SV-86723r3_rule"
   tag "stig_id": "RHEL-07-030380"
-  tag "cci": ["CCI-000126","CCI-000172"]
-  tag "nist": ["AU-2 d","AU-12 c","Rev_4"]
+  tag "cci": ["CCI-000126", "CCI-000172"]
+  tag "documentable": false
+  tag "nist": ["AU-2 d", "AU-12 c", "Rev_4"]
   tag "subsystems": ['audit', 'auditd', 'audit_rule']
   tag "check": "Verify the operating system generates audit records when
 successful/unsuccessful attempts to use the \"fchown\" command occur.
@@ -44,25 +28,32 @@ successful/unsuccessful attempts to use the \"fchown\" command occur.
 Check the file system rules in \"/etc/audit/audit.rules\" with the following
 commands:
 
-Note: The output lines of the command are duplicated to cover both 32-bit and 64-bit
-architectures. Only the lines appropriate for the system architecture must be
-present.
+Note: The output lines of the command are duplicated to cover both 32-bit and
+64-bit architectures. Only the lines appropriate for the system architecture
+must be present.
 
-# grep -i fchown /etc/audit/audit.rules
-
--a always,exit -F arch=b32 -S fchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
-
--a always,exit -F arch=b64 -S fchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
-
-If the command does not return any output, this is a finding."
-  tag "fix": "Add or update the following rule in \"/etc/audit/rules.d/audit.rules\"
-(removing those that do not match the CPU architecture):
+# grep -iw fchown /etc/audit/audit.rules
 
 -a always,exit -F arch=b32 -S fchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
 
 -a always,exit -F arch=b64 -S fchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
 
-The audit daemon must be restarted for the changes to take effect."
+If there are no audit rules defined for the \"fchown\" command, this is a
+finding.
+"
+  tag "fix": "Add or update the following rule in
+\"/etc/audit/rules.d/audit.rules\":
+
+Note: The rules are duplicated to cover both 32-bit and 64-bit architectures.
+Only the lines appropriate for the system architecture must be configured.
+
+-a always,exit -F arch=b32 -S fchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
+
+-a always,exit -F arch=b64 -S fchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
+
+The audit daemon must be restarted for the changes to take effect.
+"
+  tag "fix_id": "F-78451r6_fix"
 
   describe auditd.syscall("fchown").where {arch == "b32"} do
     its('action.uniq') { should eq ['always'] }
