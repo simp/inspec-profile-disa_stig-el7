64e7f7bf7b2806a8e4d21750a2e9bfb90c303654
Eugene Aronne <ejaronne@gmail.com>
2018-04-30 23:26:19 -0400

    Update to RHEL7v4
    - Update from RHEL7 Rel1 to Rel4

--- controls/V-71973.rb
+++ controls/V-71973.rb
@@ -1,53 +1,28 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
-
-FILE_INTEGRITY_TOOL = attribute('file_integrity_tool', default: 'aide',
-description: 'Tool used to determine file integrity')
-FILE_INTEGRITY_INTERVAL = attribute('file_integrity_interval', default: 'weekly',
-description: 'Interval for running the file integrity tool.')
-
 control "V-71973" do
   title "A file integrity tool must verify the baseline operating system
 configuration at least weekly."
   desc  "
     Unauthorized changes to the baseline configuration could make the system
-vulnerable to various attacks or allow unauthorized access to the operating system.
-Changes to operating system configurations can have unintended side effects, some of
-which may be relevant to security.
+vulnerable to various attacks or allow unauthorized access to the operating
+system. Changes to operating system configurations can have unintended side
+effects, some of which may be relevant to security.
 
     Detecting such changes and providing an automated response can help avoid
-unintended, negative consequences that could ultimately affect the security state of
-the operating system. The operating system's Information Management Officer
-(IMO)/Information System Security Officer (ISSO) and System Administrators (SAs)
-must be notified via email and/or monitoring system trap when there is an
-unauthorized modification of a configuration item.
+unintended, negative consequences that could ultimately affect the security
+state of the operating system. The operating system's Information Management
+Officer (IMO)/Information System Security Officer (ISSO) and System
+Administrators (SAs) must be notified via email and/or monitoring system trap
+when there is an unauthorized modification of a configuration item.
   "
   impact 0.5
-
   tag "gtitle": "SRG-OS-000363-GPOS-00150"
   tag "gid": "V-71973"
   tag "rid": "SV-86597r1_rule"
   tag "stig_id": "RHEL-07-020030"
-  tag "cci": "CCI-001744"
+  tag "cci": ["CCI-001744"]
+  tag "documentable": false
   tag "nist": ["CM-3 (5)", "Rev_4"]
   tag "check": "Verify the operating system routinely checks the baseline
 configuration for unauthorized changes.
@@ -59,31 +34,30 @@ Check to see if AIDE is installed on the system with the following command:
 
 # yum list installed aide
 
-If AIDE is not installed, ask the SA how file integrity checks are performed on the
-system.
+If AIDE is not installed, ask the SA how file integrity checks are performed on
+the system.
 
 Check for the presence of a cron job running daily or weekly on the system that
-executes AIDE daily to scan for changes to the system baseline. The command used in
-the example will use a daily occurrence.
+executes AIDE daily to scan for changes to the system baseline. The command
+used in the example will use a daily occurrence.
 
-Check the \"/etc/cron.daily\" subdirectory for a \"crontab\" file controlling the
-execution of the file integrity application. For example, if AIDE is installed on
-the system, use the following command:
+Check the \"/etc/cron.daily\" subdirectory for a \"crontab\" file controlling
+the execution of the file integrity application. For example, if AIDE is
+installed on the system, use the following command:
 
 # ls -al /etc/cron.* | grep aide
 -rwxr-xr-x  1 root root        29 Nov  22  2015 aide
 
-If the file integrity application does not exist, or a \"crontab\" file does not
-exist in the \"/etc/cron.daily\" or \"/etc/cron.weekly\" subdirectories, this is a
-finding."
-  tag "fix": "Configure the file integrity tool to automatically run on the system
-at least weekly. The following example output is generic. It will set cron to run
-AIDE daily, but other file integrity tools may be used:
+If the file integrity application does not exist, or a \"crontab\" file does
+not exist in the \"/etc/cron.daily\" or \"/etc/cron.weekly\" subdirectories,
+this is a finding."
+  tag "fix": "Configure the file integrity tool to automatically run on the
+system at least weekly. The following example output is generic. It will set
+cron to run AIDE daily, but other file integrity tools may be used:
 
 # cat /etc/cron.daily/aide
-0 0 * * * /usr/sbin/aide --check | /bin/mail -s \"aide integrity check run for
-<system name>\" root@sysname.mil"
-
+0 0 * * * /usr/sbin/aide --check | /bin/mail -s \"aide integrity check run for <system name>\" root@sysname.mil"
+  tag "fix_id": "F-78325r1_fix"
   describe package(FILE_INTEGRITY_TOOL) do
     it { should be_installed }
   end
--- controls/V-71975.rb
+++ controls/V-71975.rb
@@ -1,24 +1,5 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
 
 FILE_INTEGRITY_TOOL = attribute(
   'file_integ_tool',
@@ -31,71 +12,70 @@ control "V-71975" do
 changed in an unauthorized manner."
   desc  "
     Unauthorized changes to the baseline configuration could make the system
-vulnerable to various attacks or allow unauthorized access to the operating system.
-Changes to operating system configurations can have unintended side effects, some of
-which may be relevant to security.
+vulnerable to various attacks or allow unauthorized access to the operating
+system. Changes to operating system configurations can have unintended side
+effects, some of which may be relevant to security.
 
     Detecting such changes and providing an automated response can help avoid
-unintended, negative consequences that could ultimately affect the security state of
-the operating system. The operating system's Information Management Officer
-(IMO)/Information System Security Officer (ISSO) and System Administrators (SAs)
-must be notified via email and/or monitoring system trap when there is an
-unauthorized modification of a configuration item.
+unintended, negative consequences that could ultimately affect the security
+state of the operating system. The operating system's Information Management
+Officer (IMO)/Information System Security Officer (ISSO) and System
+Administrators (SAs) must be notified via email and/or monitoring system trap
+when there is an unauthorized modification of a configuration item.
   "
   impact 0.5
-
   tag "gtitle": "SRG-OS-000363-GPOS-00150"
   tag "gid": "V-71975"
   tag "rid": "SV-86599r1_rule"
   tag "stig_id": "RHEL-07-020040"
-  tag "cci": "CCI-001744"
+  tag "cci": ["CCI-001744"]
+  tag "documentable": false
   tag "nist": ["CM-3 (5)", "Rev_4"]
   tag "check": "Verify the operating system notifies designated personnel if
 baseline configurations are changed in an unauthorized manner.
 
 Note: A file integrity tool other than Advanced Intrusion Detection Environment
-(AIDE) may be used, but the tool must be executed and notify specified individuals
-via email or an alert.
+(AIDE) may be used, but the tool must be executed and notify specified
+individuals via email or an alert.
 
 Check to see if AIDE is installed on the system with the following command:
 
 # yum list installed aide
 
-If AIDE is not installed, ask the SA how file integrity checks are performed on the
-system.
+If AIDE is not installed, ask the SA how file integrity checks are performed on
+the system.
 
-Check for the presence of a cron job running routinely on the system that executes
-AIDE to scan for changes to the system baseline. The commands used in the example
-will use a daily occurrence.
+Check for the presence of a cron job running routinely on the system that
+executes AIDE to scan for changes to the system baseline. The commands used in
+the example will use a daily occurrence.
 
-Check the \"/etc/cron.daily\" subdirectory for a \"crontab\" file controlling the
-execution of the file integrity application. For example, if AIDE is installed on
-the system, use the following commands:
+Check the \"/etc/cron.daily\" subdirectory for a \"crontab\" file controlling
+the execution of the file integrity application. For example, if AIDE is
+installed on the system, use the following commands:
 
 # ls -al /etc/cron.daily | grep aide
 -rwxr-xr-x  1 root root        32 Jul  1  2011 aide
 
-AIDE does not have a configuration that will send a notification, so the cron job
-uses the mail application on the system to email the results of the file integrity
-run as in the following example:
+AIDE does not have a configuration that will send a notification, so the cron
+job uses the mail application on the system to email the results of the file
+integrity run as in the following example:
 
 # more /etc/cron.daily/aide
-0 0 * * * /usr/sbin/aide --check | /bin/mail -s \"$HOSTNAME - Daily aide integrity
-check run\" root@sysname.mil
+0 0 * * * /usr/sbin/aide --check | /bin/mail -s \"$HOSTNAME - Daily aide integrity check run\" root@sysname.mil
 
-If the file integrity application does not notify designated personnel of changes,
-this is a finding."
+If the file integrity application does not notify designated personnel of
+changes, this is a finding."
   tag "fix": "Configure the operating system to notify designated personnel if
-baseline configurations are changed in an unauthorized manner. The AIDE tool can be
-configured to email designated personnel through the use of the cron system.
+baseline configurations are changed in an unauthorized manner. The AIDE tool
+can be configured to email designated personnel through the use of the cron
+system.
 
-The following example output is generic. It will set cron to run AIDE daily and to
-send email at the completion of the analysis.
+The following example output is generic. It will set cron to run AIDE daily and
+to send email at the completion of the analysis.
 
 # more /etc/cron.daily/aide
-0 0 * * * /usr/sbin/aide --check | /bin/mail -s \"$HOSTNAME - Daily aide integrity
-check run\" root@sysname.mil"
-
+0 0 * * * /usr/sbin/aide --check | /bin/mail -s \"$HOSTNAME - Daily aide integrity check run\" root@sysname.mil"
+  tag "fix_id": "F-78327r1_fix"
   describe package(FILE_INTEGRITY_TOOL) do
     it { should be_installed }
   end
