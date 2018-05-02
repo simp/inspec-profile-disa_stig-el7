b167ba4a156bfddef9959e12e207b0778e67e441
ejaronne <34140975+ejaronne@users.noreply.github.com>
2018-05-02 00:15:11 -0400

    Update to RHEL7v4
    - Update from RHEL7 Rel1 to Rel4

--- controls/V-72215.rb
+++ controls/V-72215.rb
@@ -1,24 +1,5 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
 
 # Should we verify the virus software installation?
 ENABLE_AV = attribute(
@@ -28,27 +9,27 @@ ENABLE_AV = attribute(
 )
 
 control "V-72215" do
-  title "The system must update the DoD-approved virus scan program every seven days
-or more frequently."
+  title "The system must update the virus scan program every seven days or more
+frequently."
   desc  "
-    Virus scanning software can be used to protect a system from penetration from
-computer viruses and to limit their spread through intermediate systems.
+    Virus scanning software can be used to protect a system from penetration
+from computer viruses and to limit their spread through intermediate systems.
 
-    The virus scanning software should be configured to check for software and virus
-definition updates with a frequency no longer than seven days. If a manual process
-is required to update the virus scan software or definitions, it must be documented
-with the Information System Security Officer (ISSO).
+    The virus scanning software should be configured to check for software and
+virus definition updates with a frequency no longer than seven days. If a
+manual process is required to update the virus scan software or definitions, it
+must be documented with the Information System Security Officer (ISSO).
   "
   impact 0.5
-  tag "severity": "medium"
   tag "gtitle": "SRG-OS-000480-GPOS-00227"
   tag "gid": "V-72215"
-  tag "rid": "SV-86839r1_rule"
+  tag "rid": "SV-86839r2_rule"
   tag "stig_id": "RHEL-07-032010"
-  tag "cci": "CCI-001668"
-  tag "nist": ["SI-3 a", "Rev_3"]
-  tag "check": "Verify the system is using a DoD-approved virus scan program and the
-virus definition file is less than seven days old.
+  tag "cci": ["CCI-001668"]
+  tag "documentable": false
+  tag "nist": ["SI-3 a", "Rev_4"]
+  tag "check": "Verify the system is using a virus scan program and the virus
+definition file is less than seven days old.
 
 Check for the presence of \"McAfee VirusScan Enterprise for Linux\" with the
 following command:
@@ -56,11 +37,12 @@ following command:
 # systemctl status nails
 nails - service for McAfee VirusScan Enterprise for Linux
 >  Loaded: loaded
-/opt/NAI/package/McAfeeVSEForLinux/McAfeeVSEForLinux-2.0.2.<build_number>; enabled)
+/opt/NAI/package/McAfeeVSEForLinux/McAfeeVSEForLinux-2.0.2.<build_number>;
+enabled)
 >  Active: active (running) since Mon 2015-09-27 04:11:22 UTC;21 min ago
 
-If the \"nails\" service is not active, check for the presence of \"clamav\" on the
-system with the following command:
+If the \"nails\" service is not active, check for the presence of \"clamav\" on
+the system with the following command:
 
 # systemctl status clamav-daemon.socket
 systemctl status clamav-daemon.socket
@@ -77,8 +59,8 @@ dates of the virus definition files with the following command:
 If the virus definition files have dates older than seven days from the current
 date, this is a finding.
 
-If \"clamav\" is active on the system, check the dates of the virus database with
-the following commands:
+If \"clamav\" is active on the system, check the dates of the virus database
+with the following commands:
 
 # grep -I databasedirectory /etc/clamav.conf
 DatabaseDirectory /var/lib/clamav
@@ -86,10 +68,10 @@ DatabaseDirectory /var/lib/clamav
 # ls -al /var/lib/clamav/*.cvd
 -rwxr-xr-x  1 root root      149156 Mar  5  2011 daily.cvd
 
-If the database file has a date older than seven days from the current date, this is
-a finding."
-  tag "fix": "Update the approved DoD virus scan software and virus definition
-files."
+If the database file has a date older than seven days from the current date,
+this is a finding."
+  tag "fix": "Update the virus scan software and virus definition files."
+  tag "fix_id": "F-78569r2_fix"
 
   sec_per_wk = 604800
 
