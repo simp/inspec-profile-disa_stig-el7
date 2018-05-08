2e02ebc8a4254a00a51ba87ee657ede971145cbd
root <root@inspec-dev.mitre.org>
2018-05-08 11:16:03 -0400

    Updated to include new audit rule checks.

--- controls/V-72171.rb
+++ controls/V-72171.rb
@@ -70,8 +70,25 @@ The audit daemon must be restarted for the changes to take effect."
     its('action.uniq') { should eq ['always'] }
     its('list.uniq') { should eq ['exit'] }
   end
+  describe auditd.path("/bin/mount").where {arch == "b32"} do
+    its('action.uniq') { should eq ['always'] }
+    its('list.uniq') { should eq ['exit'] }
+  end
+  describe auditd.path("/usr/bin/mount").where {arch == "b32"} do
+    its('action.uniq') { should eq ['always'] }
+    its('list.uniq') { should eq ['exit'] }
+  end
+  
   if os.arch == 'x86_64'
     describe auditd.syscall("mount").where {arch == "b64"} do
+      its('action.uniq') { should eq ['always'] }
+      its('list.uniq') { should eq ['exit'] }
+    end
+    describe auditd.path("/bin/mount").where {arch == "b64"} do
+      its('action.uniq') { should eq ['always'] }
+      its('list.uniq') { should eq ['exit'] }
+    end
+    describe auditd.path("/usr/bin/mount").where {arch == "b64"} do
       its('action.uniq') { should eq ['always'] }
       its('list.uniq') { should eq ['exit'] }
     end
