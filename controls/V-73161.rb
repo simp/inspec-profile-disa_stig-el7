aea63e62c58b64518346c8f10c5dd51dac4a2c01
root <root@inspec-dev.mitre.org>
2018-05-15 11:34:46 -0400

    updating controls so they don't evaluate to not_tested.

--- controls/V-73161.rb
+++ controls/V-73161.rb
@@ -35,9 +35,16 @@ systems that are being exported via NFS."
   tag "fix_id": "F-79607r1_fix"
 
   nfs_systems = etc_fstab.nfs_file_systems.entries
-  nfs_systems.each do |file_system|
-    describe file_system do
-      its ('mount_options') { should include 'noexec' }
+  if !nfs_systems.nil? and !nfs_systems.empty?
+    nfs_systems.each do |file_system|
+      describe file_system do
+        its ('mount_options') { should include 'noexec' }
+      end
+    end
+  else
+    describe "No NFS file systems were found." do
+      subject { nfs_systems.nil? or nfs_systems.empty? }
+      it { should eq true }
     end
   end
 end
