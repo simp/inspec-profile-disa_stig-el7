5c5b56e83df5f114664a8dc1af96e2e0acae8b49
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-06-23 21:45:35 -0400

    Fixes and Rev4 updates for audit controls (#21)
    * Updated to use parse_config_file.
    * Updated to align with the check in V-72087.
    * Updated CCI and NIST tags to use arrays.
    * updated impact logic to account for Not Applicable cases
    * used the protected regex pattern `%r{ }` in place is //
    * fixed many false positives reported by end users
    * added TODOs to all controls that need to be enhanced
    * added a new attributes files for this branch for review
    * added logic so that all controls correctly report
    * removed most `only_if` statments and replaced with skips + impact 0.0
    * removed all `severity` tags in lue of using `impact`
    * Removed extra attribute defn
    * Fixed typo in call to package resource
    * updated V-71961 to account for multiple users
    * added attributes to support each
    * Adds tests for control V-72083
    * Fixed control V-72083 audisp check to use valid regex and check for localhost or 127.0.0.1
    * Update to RHEL7v4
    * commenting out over-reaching checks in the control. can discuss later.
    * added check for commented out line.
    * updating so it doesn't evaluate to not_tested.
    * Fixed its() clause bug
    * Modified match statement to use %r for consistency
    * Fixed control V-72081 attribute monitor_kernel_log to be not a ruby constant and also fixed impact logic

--- controls/V-72209.rb
+++ controls/V-72209.rb
@@ -1,24 +1,5 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
 
 log_pkg_path = attribute(
   'log_pkg_path',
@@ -28,16 +9,16 @@ log_pkg_path = attribute(
 
 control "V-72209" do
   title "The system must send rsyslog output to a log aggregation server."
-  desc  "Sending rsyslog output to another system ensures that the logs cannot be
-removed or modified in the event that the system is compromised or has a hardware
-failure."
+  desc  "Sending rsyslog output to another system ensures that the logs cannot
+be removed or modified in the event that the system is compromised or has a
+hardware failure."
   impact 0.5
-  tag "severity": "medium"
   tag "gtitle": "SRG-OS-000480-GPOS-00227"
   tag "gid": "V-72209"
   tag "rid": "SV-86833r1_rule"
   tag "stig_id": "RHEL-07-031000"
-  tag "cci": "CCI-000366"
+  tag "cci": ["CCI-000366"]
+  tag "documentable": false
   tag "nist": ["CM-6 b", "Rev_4"]
   tag "subsystems": ['audit', 'auditd', 'rsyslog']
   tag "check": "Verify \"rsyslog\" is configured to send all messages to a log
@@ -45,25 +26,27 @@ aggregation server.
 
 Check the configuration of \"rsyslog\" with the following command:
 
-Note: If another logging package is used, substitute the utility configuration file
-for \"/etc/rsyslog.conf\".
+Note: If another logging package is used, substitute the utility configuration
+file for \"/etc/rsyslog.conf\".
 
 # grep @ /etc/rsyslog.conf
 *.* @@logagg.site.mil
 
-If there are no lines in the \"/etc/rsyslog.conf\" file that contain the \"@\" or
-\"@@\" symbol(s), and the lines with the correct symbol(s) to send output to another
-system do not cover all \"rsyslog\" output, ask the System Administrator to indicate
-how the audit logs are off-loaded to a different system or media.
+If there are no lines in the \"/etc/rsyslog.conf\" file that contain the \"@\"
+or \"@@\" symbol(s), and the lines with the correct symbol(s) to send output to
+another system do not cover all \"rsyslog\" output, ask the System
+Administrator to indicate how the audit logs are off-loaded to a different
+system or media.
 
-If there is no evidence that the audit logs are being sent to another system, this
-is a finding."
-  tag "fix": "Modify the \"/etc/rsyslog.conf\" file to contain a configuration line
-to send all \"rsyslog\" output to a log aggregation system:
+If there is no evidence that the audit logs are being sent to another system,
+this is a finding."
+  tag "fix": "Modify the \"/etc/rsyslog.conf\" file to contain a configuration
+line to send all \"rsyslog\" output to a log aggregation system:
 
 *.* @@<log aggregation system name>"
+  tag "fix_id": "F-78563r1_fix"
 
-  describe command("grep @ #{log_pkg_path}") do
+  describe command("grep @ #{log_pkg_path} | grep -v \"^#\"") do
     its('stdout.strip') { should_not be_empty }
   end
 end
