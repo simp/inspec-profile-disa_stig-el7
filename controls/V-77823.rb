6a524867568554ff5fcc270d295a00611b8db10c
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-06-23 23:10:43 -0400

    Removed incorrect check for gnome-desktop3 (control has nothing to do with gnome)
    Signed-off-by: Sam Cornwell <14048146+samcornwell@users.noreply.github.com>

--- controls/V-77823.rb
+++ controls/V-77823.rb
@@ -41,10 +41,5 @@ default\"
 
   describe command("grep -i execstart /usr/lib/systemd/system/rescue.service") do
     its('stdout.strip') { should match %r{/usr/sbin/sulogin} }
-  end if package('gnome-desktop3').installed?
-  describe "The GNOME desktop is not installed" do
-    skip "The GNOME desktop is not installed, this control is Not Applicable."
-  end if !package('gnome-desktop3').installed?
-
-
+  end
 end
