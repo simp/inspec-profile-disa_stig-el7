02f18b8172613a4464d2cb3067c5b692a2114ace
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-07-16 20:37:09 -0400

    Rewrote V-72075 to check the root partition is not removable
    Signed-off-by: Sam Cornwell <14048146+samcornwell@users.noreply.github.com>

--- controls/V-72075.rb
+++ controls/V-72075.rb
@@ -49,14 +49,27 @@ finding."
 media or document the configuration to boot from removable media with the ISSO."
   tag "fix_id": "F-78427r1_fix"
 
-  describe "The list of unapproved boot loader configuration files" do
-    subject {
-      command('find / -name grub.cfg -type f').stdout.chomp.split
-    }
-    before {
-      subject.delete("/boot/grub2/grub.cfg")
-      subject.delete("/boot/efi/EFI/redhat/grub.cfg")
-    }
-    its ('length') { should == 0 }
-  end
+  roots = command('grubby --info=ALL | grep "^root=" | sed "s/^root=//g"').
+    stdout.strip.split("\n")
+
+  blocks = roots.map { |root|
+    root_file = file(root)
+    root_file.symlink? ? root_file.link_path : root_file.path
+  }.uniq
+
+  blocks.each { |block|
+    block_file = file(block)
+    describe block_file do
+      it { should exist }
+      its('path') { should match %r{^/dev/} }
+    end
+
+    if block_file.exist? and block_file.path.match? %r{^/dev/}
+      removable = ['/sys/block', block.sub(%r{^/dev/}, ''), 'removable'].join('/')
+      describe file(removable) do
+        it { should exist }
+        its('content.strip') { should eq '0' }
+      end
+    end
+  }
 end
