534232d06ab22c572b8b01bfa0b7096f2ceca879
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-07-30 09:38:45 -0400

    Fixes and Rev4 updates for ssh controls (#25)
    * Updated CCI and NIST tags to use arrays.
    * * Reviewed all controls and fixed many issues
    * updated impact logic to account for Not Applicable cases
    * used the protected regex pattern `%r{ }` in place is //
    * fixed many false positives reported by end users
    * added TODOs to all controls that need to be enhanced
    * added a new attributes files for this branch for review
    * added logic so that all controls correctly report
    * removed most `only_if` statements and replaced with skips + impact 0.0
    * removed all `severity` tags in lue of using `impact`
    ** Added EXEMPT_HOME_USERS attribute attributes file
    * Removed extra attribute defn
    * Fixed typo in call to package resource
    * Updated attributes file
    * Updating to do a range check and if the ClientAliveInterval is not set
    * updating controls so they don't evaluate to not_tested.
    * Fixed some ssh controls to be more explicit
    * Replaced constant attribute CLIENT_ALIVE_INTERVAL with non-constant

--- controls/V-72235.rb
+++ controls/V-72235.rb
@@ -1,80 +1,57 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
-
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
-
 control "V-72235" do
-  title "All networked systems must use SSH for confidentiality and integrity of
-transmitted and received information as well as information during preparation for
-transmission."
+  title "All networked systems must use SSH for confidentiality and integrity
+of transmitted and received information as well as information during
+preparation for transmission."
   desc  "
-    Without protection of the transmitted information, confidentiality and integrity
-may be compromised because unprotected communications can be intercepted and either
-read or altered.
+    Without protection of the transmitted information, confidentiality and
+integrity may be compromised because unprotected communications can be
+intercepted and either read or altered.
 
-    This requirement applies to both internal and external networks and all types of
-information system components from which information can be transmitted (e.g.,
-servers, mobile devices, notebook computers, printers, copiers, scanners, and
-facsimile machines). Communication paths outside the physical protection of a
-controlled boundary are exposed to the possibility of interception and modification.
+    This requirement applies to both internal and external networks and all
+types of information system components from which information can be
+transmitted (e.g., servers, mobile devices, notebook computers, printers,
+copiers, scanners, and facsimile machines). Communication paths outside the
+physical protection of a controlled boundary are exposed to the possibility of
+interception and modification.
 
-    Protecting the confidentiality and integrity of organizational information can
-be accomplished by physical means (e.g., employing physical distribution systems) or
-by logical means (e.g., employing cryptographic techniques). If physical means of
-protection are employed, then logical means (cryptography) do not have to be
-employed, and vice versa.
-
-    Satisfies: SRG-OS-000423-GPOS-00187, SRG-OS-000423-GPOS-00188,
-SRG-OS-000423-GPOS-00189, SRG-OS-000423-GPOS-0019.
+    Protecting the confidentiality and integrity of organizational information
+can be accomplished by physical means (e.g., employing physical distribution
+systems) or by logical means (e.g., employing cryptographic techniques). If
+physical means of protection are employed, then logical means (cryptography) do
+not have to be employed, and vice versa.
   "
   impact 0.5
-  tag "severity": "medium"
   tag "gtitle": "SRG-OS-000423-GPOS-00187"
+  tag "satisfies": ["SRG-OS-000423-GPOS-00187", "SRG-OS-000423-GPOS-00188",
+"SRG-OS-000423-GPOS-00189", "SRG-OS-000423-GPOS-00190"]
   tag "gid": "V-72235"
   tag "rid": "SV-86859r2_rule"
   tag "stig_id": "RHEL-07-040310"
-  tag "cci": "CCI-002418"
-  tag "nist": ["SC-8", "Rev_4"]
-  tag "cci": "CCI-002420"
-  tag "nist": ["SC-8 (2)", "Rev_4"]
-  tag "cci": "CCI-002421"
-  tag "nist": ["SC-8 (1)", "Rev_4"]
-  tag "cci": "CCI-002422"
-  tag "nist": ["SC-8 (2)", "Rev_4"]
+  tag "cci": ["CCI-002418", "CCI-002420", "CCI-002421", "CCI-002422"]
+  tag "documentable": false
+  tag "nist": ["SC-8", "SC-8 (2)", "SC-8 (1)", "SC-8 (2)", "Rev_4"]
   tag "subsystems": ["ssh"]
   tag "check": "Verify SSH is loaded and active with the following command:
 
 # systemctl status sshd
  sshd.service - OpenSSH server daemon
    Loaded: loaded (/usr/lib/systemd/system/sshd.service; enabled)
-   Active: active (running) since Tue 2015-11-17 15:17:22 EST; 4 weeks 0 days ago
+   Active: active (running) since Tue 2015-11-17 15:17:22 EST; 4 weeks 0 days
+ago
  Main PID: 1348 (sshd)
    CGroup: /system.slice/sshd.service
            ??1348 /usr/sbin/sshd -D
 
-If \"sshd\" does not show a status of \"active\" and \"running\", this is a finding."
-  tag "fix": "Configure the SSH service to automatically start after reboot with the
-following command:
+If \"sshd\" does not show a status of \"active\" and \"running\", this is a
+finding."
+  tag "fix": "Configure the SSH service to automatically start after reboot
+with the following command:
 
 # systemctl enable sshd ln -s '/usr/lib/systemd/system/sshd.service'
 '/etc/systemd/system/multi-user.target.wants/sshd.service'"
+  tag "fix_id": "F-78589r1_fix"
 
   describe systemd_service('sshd.service') do
     it { should be_running }
