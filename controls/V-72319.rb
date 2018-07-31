8633692e68b49fb24f890fee4cc251e7cc7e593c
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-07-31 15:04:05 -0400

    Fixes and Rev4 updates for network controls (#33)
    * Updated to include iptables check.
    * Updated to account for IPv6 being disabled in the kernel.
    * Updated CCI and NIST tags to use arrays.
    * fixed missing [ in cci tag.
    * Pulling in changes from csp_mod branch
    * * Reviewed all controls and fixed many issues
    * updated impact logic to account for Not Applicable cases
    * used the protected regex pattern `%r{ }` in place is //
    * fixed many false positives reported by end users
    * added TODOs to all controls that need to be enhanced
    * added a new attributes files for this branch for review
    * added logic so that all controls correctly report
    * removed most `only_if` statments and replaced with skips + impact 0.0
    * removed all `severity` tags in lue of using `impact`
    * * Added EXEMPT_HOME_USERS attribute attributes file
    * Removed extra attribute defn
    * Fixed typo in call to package resource
    * Updated attributes file
    * small change to push file
    * * updated V-71863 fix the banner compare
    * updated V-71961 to account for multiple users
    * added attrubutes to support each
    * * Added check in V-72223 for TMOUT is in the bashrc file
    * Corrected attribute name in V-72223
    * Merged attributes files
    * * Updated control V-72317
    * * fixed small / major issues with some of the tests. * enhanced / corrected the Not Applicable reporting. * ensured all 'Not Applicable' by attribute controls worked fully. * small fixes to attributes * enhamcements to completeness of controls and testing
    * Update to RHEL7v4
    * Updated to support checking the files in /etc/profile.d
    * Fixed V-72269 to properly check that maxpoll is set
    * Fixed control V-72223 to describe.one on all the possible locations of TMOUT
    * Removed constant attribute from V-72223
    * Added xautofs flag to a few find commands
    * Rewrote SNMP control V-72313 to recursively find all conf files via include statements
    * Rewrote ipsec control V-72317 to recursively find all conf files via include
    * Changed ntpd maxpoll control to use ntpd --saveconfigquit (currently loaded config) rather than the config file
    * Added dangerous global find tag to couple controls

--- controls/V-72319.rb
+++ controls/V-72319.rb
@@ -1,39 +1,22 @@
 # encoding: utf-8
 #
-=begin
------------------
-Benchmark: Red Hat Enterprise Linux 7 Security Technical Implementation Guide
-Status: Accepted
 
-This Security Technical Implementation Guide is published as a tool to improve
-the security of Department of Defense (DoD) information systems. The
-requirements are derived from the National Institute of Standards and
-Technology (NIST) 800-53 and related documents. Comments or proposed revisions
-to this document should be sent via email to the following address:
-disa.stig_spt@mail.mil.
-
-Release Date: 2017-03-08
-Version: 1
-Publisher: DISA
-Source: STIG.DOD.MIL
-uri: http://iase.disa.mil
------------------
-=end
+# TODO we need to account for the case when IPV6 is not disabled and check it
 
 control "V-72319" do
   title "The system must not forward IPv6 source-routed packets."
   desc  "Source-routed packets allow the source of the packet to suggest that
-routers forward the packet along a different path than configured on the router,
-which can be used to bypass network security measures. This requirement applies only
-to the forwarding of source-routed traffic, such as when IPv6 forwarding is enabled
-and the system is functioning as a router."
+routers forward the packet along a different path than configured on the
+router, which can be used to bypass network security measures. This requirement
+applies only to the forwarding of source-routed traffic, such as when IPv6
+forwarding is enabled and the system is functioning as a router."
   impact 0.5
-  tag "severity": "medium"
   tag "gtitle": "SRG-OS-000480-GPOS-00227"
   tag "gid": "V-72319"
   tag "rid": "SV-86943r1_rule"
   tag "stig_id": "RHEL-07-040830"
-  tag "cci": "CCI-000366"
+  tag "cci": ["CCI-000366"]
+  tag "documentable": false
   tag "nist": ["CM-6 b", "Rev_4"]
   tag "networking","kernel"
   tag "check": "Verify the system does not accept IPv6 source-routed packets.
@@ -45,16 +28,22 @@ Check the value of the accept source route variable with the following command:
 # /sbin/sysctl -a | grep  net.ipv6.conf.all.accept_source_route
 net.ipv6.conf.all.accept_source_route=0
 
-If the returned lines do not have a value of \"0\", or a line is not returned, this
-is a finding."
-  tag "fix": "Set the system to the required kernel parameter, if IPv6 is enabled,
-by adding the following line to \"/etc/sysctl.conf\" (or modify the line to have the
-required value):
+If the returned lines do not have a value of \"0\", or a line is not returned,
+this is a finding."
+  tag "fix": "Set the system to the required kernel parameter, if IPv6 is
+enabled, by adding the following line to \"/etc/sysctl.conf\" (or modify the
+line to have the required value):
 
 net.ipv6.conf.all.accept_source_route = 0"
+  tag "fix_id": "F-78673r1_fix"
 
-  describe kernel_parameter('net.ipv6.conf.all.accept_source_route') do
-    its('value') { should eq 0 }
+  describe.one do
+    describe kernel_parameter('net.ipv6.conf.all.accept_source_route') do
+      its('value') { should eq 0 }
+    end
+	# If IPv6 is disabled in the kernel it will return NIL
+    describe kernel_parameter('net.ipv6.conf.all.accept_source_route') do
+      its('value') { should eq nil }
+    end
   end
-
 end
