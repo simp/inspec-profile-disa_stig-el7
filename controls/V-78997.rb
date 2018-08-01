c60ddb7eb89c45ccb67b7a52a8ceb4e0fd560c31
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-08-01 10:50:15 -0400

    Fixes and Rev4 updates for session lock controls (#26)
    * Update from RHEL7 Rel1 to Rel4
    * Updating logic to extract value so it can be evaluated using comparison operators.
    * Fixing smart double quotes to be ascii double quotes
    * updating controls so they don't evaluate to not_tested.
    * Improved gnome controls to use gsettings rather than hard-coded file paths

--- controls/V-78997.rb
+++ controls/V-78997.rb
@@ -1,19 +1,20 @@
 # encoding: utf-8
 #
-
-control "V-73155" do
+control "V-78997" do
   title "The operating system must prevent a user from overriding the
-screensaver lock-delay setting for the graphical user interface."
+screensaver idle-activation-enabled setting for the graphical user interface."
   desc  "
-    A session time-out lock is a temporary action taken when a user stops work
-and moves away from the immediate physical vicinity of the information system
-but does not log out because of the temporary nature of the absence. Rather
-than relying on the user to manually lock their operating system session prior
-to vacating the vicinity, operating systems need to be able to identify when a
-user's session has idled and take action to initiate the session lock.
+    A session lock is a temporary action taken when a user stops work and moves
+away from the immediate physical vicinity of the information system but does
+not want to log out because of the temporary nature of the absence.
 
     The session lock is implemented at the point where session activity can be
-determined and/or controlled.
+determined.
+
+    The ability to enable/disable a session lock is given to the user by
+default. Disabling the user’s ability to disengage the graphical user interface
+session lock provides the assurance that all sessions will lock after the
+specified period of time.
   "
   if package('gnome-desktop3').installed?
     impact 0.5
@@ -21,15 +22,14 @@ determined and/or controlled.
     impact 0.0
   end
   tag "gtitle": "SRG-OS-000029-GPOS-00010"
-  tag "gid": "V-73155"
-  tag "rid": "SV-87807r3_rule"
-  tag "stig_id": "RHEL-07-010081"
+  tag "gid": "V-78997"
+  tag "rid": "SV-93703r1_rule"
+  tag "stig_id": "RHEL-07-010101"
   tag "cci": ["CCI-000057"]
   tag "documentable": false
   tag "nist": ["AC-11 a", "Rev_4"]
-  tag "check": "Verify the operating system prevents a user from overriding a
-screensaver lock after a 15-minute period of inactivity for graphical user
-interfaces.
+  tag "check": "Verify the operating system prevents a user from overriding the
+screensaver idle-activation-enabled setting for the graphical user interface.
 
 Note: If the system does not have GNOME installed, this requirement is Not
 Applicable. The screen program must be installed to lock sessions on the
@@ -40,15 +40,15 @@ Determine which profile the system database is using with the following command:
 
 system-db:local
 
-Check for the lock delay setting with the following command:
+Check for the idle-activation-enabled setting with the following command:
 
 Note: The example below is using the database \"local\" for the system, so the
 path is \"/etc/dconf/db/local.d\". This path must be modified if a database
 other than \"local\" is being used.
 
-# grep -i lock-delay /etc/dconf/db/local.d/locks/*
+# grep -i idle-activation-enabled /etc/dconf/db/local.d/locks/*
 
-/org/gnome/desktop/screensaver/lock-delay
+/org/gnome/desktop/screensaver/idle-activation-enabled
 
 If the command does not return a result, this is a finding."
   tag "fix": "Configure the operating system to prevent a user from overriding
@@ -64,12 +64,12 @@ should be created under the appropriate subdirectory.
 
 # touch /etc/dconf/db/local.d/locks/session
 
-Add the setting to lock the screensaver lock delay:
+Add the setting to lock the screensaver idle-activation-enabled setting:
 
-/org/gnome/desktop/screensaver/lock-delay"
-  tag "fix_id": "F-79601r2_fix"
+/org/gnome/desktop/screensaver/idle-activation-enabled"
+  tag "fix_id": "F-85747r1_fix"
 
-  describe command("gsettings writable org.gnome.desktop.screensaver lock-delay") do
+  describe command("gsettings writable org.gnome.desktop.screensaver idle-activation-enabled") do
     its('stdout.strip') { should cmp 'false' }
   end if package('gnome-desktop3').installed?
 
