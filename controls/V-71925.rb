77e4d2457d317a2c212914bf9a8ce37567b4e141
ischmidt1235 <51722767+ischmidt1235@users.noreply.github.com>
2019-07-23 12:40:03 -0400

    Update V-71925.rb

--- controls/V-71925.rb
+++ controls/V-71925.rb
@@ -19,7 +19,7 @@ defeat the organization's policy regarding password reuse."
   tag "cci": ["CCI-000198"]
   tag "documentable": false
   tag "nist": ["IA-5 (1) (d)", "Rev_4"]
-  tag "subsystems": ['useradd', 'password']
+  tag "subsystems": ['login_defs', 'password']
   tag "check": "Verify the operating system enforces 24 hours/1 day as the
 minimum password lifetime for new user accounts.
 
