e1c99d644a782862f79ed91710f00394577568c2
ischmidt1235 <51722767+ischmidt1235@users.noreply.github.com>
2019-07-31 14:51:32 -0400

    Added subsystems tags
    file_system, nfs, autofs

--- controls/V-71985.rb
+++ controls/V-71985.rb
@@ -14,6 +14,7 @@ unknown devices, thereby facilitating malicious activity."
   tag "cci": ["CCI-000366", "CCI-000778", "CCI-001958"]
   tag "documentable": false
   tag "nist": ["CM-6 b", "IA-3", "IA-3", "Rev_4"]
+  tag "subsystems": ['file_system', 'nfs', 'autofs']
   tag "check": "Verify the operating system disables the ability to automount
 devices.
 
