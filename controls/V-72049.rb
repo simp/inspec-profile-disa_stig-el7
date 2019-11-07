d417e58fec1fb35d0752b012829078216b046c9f
Danny Haynes <dhaynes@mitre.org>
2019-11-07 09:04:51 -0500

    Update V-72049.rb

--- controls/V-72049.rb
+++ controls/V-72049.rb
@@ -1,19 +1,5 @@
 # encoding: utf-8
 #
-
-disable_slow_controls = input(
-  'disable_slow_controls',
-  value: false,
-  description: 'If enabled, this attribute disables this control and other                                                                                   
-                controls that consistently take a long time to complete.'
-)
-
-exempt_home_users = input(
-  'exempt_home_users',
-  description: 'These are `home dir` exempt interactive accounts',
-  value: []
-)
-
 non_interactive_shells = input(
   'non_interactive_shells',
   description: 'These shells do not allow a user to login',
