adae565b200152609be1b41097481c756d5fe357
Aaron Lippold <lippold@gmail.com>
2020-12-04 19:45:47 -0500

    fixed location of if statement fixed regex escaping
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/V-72433.rb
+++ controls/V-72433.rb
@@ -1,5 +1,5 @@
-# -*- encoding : utf-8 -*-
-control "V-72433" do
+
+control 'V-72433' do
   title "The Red Hat Enterprise Linux operating system must implement
 certificate status checking for PKI authentication."
   desc  "Using an authentication device, such as a CAC or token that is
@@ -28,8 +28,8 @@ configuring the device itself (management).
 
 
   "
-  desc  "rationale", ""
-  desc  "check", "
+  desc  'rationale', ''
+  desc  'check', "
     Verify the operating system implements certificate status checking for PKI
 authentication.
 
@@ -47,7 +47,7 @@ system with the following command:
     If \"ocsp_on\" is not present in all uncommented \"cert_policy\" lines in
 \"/etc/pam_pkcs11/pam_pkcs11.conf\", this is a finding.
   "
-  desc  "fix", "
+  desc 'fix', "
     Configure the operating system to do certificate status checking for PKI
 authentication.
 
@@ -55,34 +55,34 @@ authentication.
 \"/etc/pam_pkcs11/pam_pkcs11.conf\" to include \"ocsp_on\".
   "
   tag severity: nil
-  tag gtitle: "SRG-OS-000375-GPOS-00160"
-  tag satisfies: ["SRG-OS-000375-GPOS-00160", "SRG-OS-000375-GPOS-00161",
-"SRG-OS-000375-GPOS-00162"]
-  tag gid: "V-72433"
-  tag rid: "SV-87057r5_rule"
-  tag stig_id: "RHEL-07-041003"
-  tag fix_id: "F-78785r3_fix"
-  tag cci: ["CCI-001948", "CCI-001953", "CCI-001954"]
-  tag nist: ["IA-2 (11)", "IA-2 (12)", "IA-2 (12)", "Rev_4"]
+  tag gtitle: 'SRG-OS-000375-GPOS-00160'
+  tag satisfies: ['SRG-OS-000375-GPOS-00160', 'SRG-OS-000375-GPOS-00161',
+                  'SRG-OS-000375-GPOS-00162']
+  tag gid: 'V-72433'
+  tag rid: 'SV-87057r5_rule'
+  tag stig_id: 'RHEL-07-041003'
+  tag fix_id: 'F-78785r3_fix'
+  tag cci: ['CCI-001948', 'CCI-001953', 'CCI-001954']
+  tag nist: ['IA-2 (11)', 'IA-2 (12)', 'IA-2 (12)', 'Rev_4']
 
   smart_card_status = input('smart_card_status')
 
   if smart_card_status.eql?('enabled')
     impact 0.5
-    if ((pam_file = file('/etc/pam_pkcs11/pam_pkcs11.conf')).exist?)
-      cert_policy_lines = (pam_file.content.nil?)?[]:
-        pam_file.content.lines.grep(%r{^(?!.+#).*cert_policy}i)
-      if (cert_policy_lines.length < 3)
-        describe "should contain at least 3 cert policy lines" do
+    if (pam_file = file('/etc/pam_pkcs11/pam_pkcs11.conf')).exist?
+      cert_policy_lines = pam_file.content.nil?   ? [] :
+        pam_file.content.lines.grep(/^(?!.+#).*cert_policy/i)
+      if cert_policy_lines.length < 3
+        describe 'should contain at least 3 cert policy lines' do
           subject { cert_policy_lines.length }
           it { should >= 3 }
         end
       else
-        describe "each cert policy line should include oscp_on" do
+        describe 'each cert policy line should include oscp_on' do
           cert_policy_lines.each do |line|
-           subject { line }
-           it { should match %r{=[^;]*ocsp_on}i }
-	  end
+          subject { line }
+            it { should match %r{=[^;]*ocsp_on}i }
+          end
         end
       end
     else
@@ -92,9 +92,8 @@ authentication.
     end
   else
     impact 0.0
-    describe "The system is not smartcard enabled" do
-      skip "The system is not using Smartcards / PIVs to fulfil the MFA requirement, this control is Not Applicable."
+    describe 'The system is not smartcard enabled' do
+      skip 'The system is not using Smartcards / PIVs to fulfil the MFA requirement, this control is Not Applicable.'
     end
   end
 end
-
